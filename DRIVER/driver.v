0e25b1793a21b138e28f8be53c2bfaa84b0405d2
KeerthiPatil-RTL <keerthipatil2001@gmail.com>
2025-11-23 19:34:39 -0500

    Update driver.v

--- DRIVER/driver.v
+++ DRIVER/driver.v
@@ -1,1 +1,11 @@
-
+module driver #(parameter WIDTH = 8) 
+(input [WIDTH -1 :0] data_in,
+input data_en,
+output reg [WIDTH -1 :0] data_out);
+always@(*) begin
+if(data_en == 1'b1)
+data_out = data_in;
+else 
+data_out = {WIDTH{1'bx}};
+end
+endmodule
